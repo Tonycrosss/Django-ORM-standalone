e8bd5001eb80f7e1faa15b55d3a31576698c1a9d
mpak <starovoitov.anton@gmail.com>
2019-12-31 10:31:51 +0500

    added filtering passcards for active only

--- main.py
+++ main.py
@@ -10,4 +10,7 @@ from datacenter.models import Passcard, Visit
 
 if __name__ == "__main__":
     # Программируем здесь
+    all_passcards_list = [Passcard.objects.all()]
+    active_passcards_list = [Passcard.objects.filter(is_active=True)]
     print('Количество пропусков:', Passcard.objects.count())
+    print('Количество активных пропусков:', len(active_passcards_list[0]))
